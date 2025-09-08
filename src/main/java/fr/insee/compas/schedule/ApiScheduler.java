6a3c99fc0fe5fe0bd038887557c533486f3ce00b
Colin Despouys <colin.despouys@insee.fr>
2025-09-08 15:19:47 +0200

    lancement de la recuperation a 4h

--- src/main/java/fr/insee/compas/schedule/ApiScheduler.java
+++ src/main/java/fr/insee/compas/schedule/ApiScheduler.java
@@ -34,7 +34,7 @@ public class ApiScheduler {
         this.updateIndicatorDevopsService = updateIndicatorDevopsService;
     }
 
-    @Scheduled(cron = "0 0 22 * * *")
+    @Scheduled(cron = "0 0 4 * * *")
     public void callApi() {
         log.info("mise à jour des modules oscar à la date de {}", LocalDate.now());
         oscarService.miseAjourModuleOscarEnBaseDeDonnees();
